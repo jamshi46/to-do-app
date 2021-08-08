f25006d1f75d9a05ecf4c2a9b6282a64e4a052c4
jamshi46 <63713251+jamshi46@users.noreply.github.com>
2021-08-08 21:22:50 +0530

    Add files via upload

---
+++ To-Do-App/javascript.js
@@ -0,0 +1,26 @@
+let toDoAdd = document.querySelector("#todoadd");
+let toDoInput = document.querySelector("#todoinput");
+let toDoBox = document.querySelector("#todobox");
+
+
+
+toDoAdd.addEventListener('click',function() {
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-css')
+    paragraph.id = 'todo'
+    paragraph.innerText=toDoInput.value;
+    toDoBox.appendChild(paragraph);
+    toDoInput.value= "";
+    var checkbox = document.createElement('input');
+    checkbox.classList.add('checkbox-css')
+    checkbox.type ='checkbox';
+    paragraph.appendChild(checkbox);
+   var deleted = document.createElement('button');
+   deleted.classList.add('delete-css')
+    deleted.innerHTML = '<i class="fas fa-trash"></i>';
+   paragraph.appendChild(deleted);
+   deleted.addEventListener('click',function() {
+   var deleting = document.getElementById('todo');
+    deleting.remove();
+});
+});
